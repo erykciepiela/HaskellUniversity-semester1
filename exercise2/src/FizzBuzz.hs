1d66c403fbcccbd869376a431551a16474a3d38f
Eryk Ciepiela <eryk.ciepiela.dev@gmail.com>
2017-12-09 17:02:50 +0100

    exercise 2

--- exercise2/src/FizzBuzz.hs
+++ exercise2/src/FizzBuzz.hs
@@ -3,13 +3,22 @@ module FizzBuzz
     ) where
 
 import System.Environment
+import Data.Maybe
 
 program :: IO ()
-program = print $ fmap fizzbuzz [1..20]
+program = print $ answer [each 3 "fizz", each 5 "buzz"] show <$> [1..20]
 
-fizzbuzz :: Int -> String
-fizzbuzz i
-  | i `mod` 15 == 0 = "fizzbuzz"
-  | i `mod` 3 == 0 = "fizz"
-  | i `mod` 5 == 0 = "buzz"
-  | otherwise = show i
+each :: Int -> String -> Int -> Maybe String
+each j s i = if i `mod` j == 0 then Just s else Nothing
+
+answer :: [Int -> Maybe String] -> (Int -> String) -> Int -> String
+answer f d i = let x = fmap ($ i) f in fromMaybe (d i) (xxx x)
+  where
+    xxx :: [Maybe String] -> Maybe String
+    xxx [] = Nothing
+    xxx (Just s:mss) = case xxx mss of
+      Nothing -> Just s
+      Just ss -> Just (s ++ ss)
+    xxx (Nothing:mss) = case xxx mss of
+      Nothing -> Nothing
+      Just ss -> Just ss
