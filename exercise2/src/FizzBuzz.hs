27c5fee69b83b57ef2cf7552a831294707231e22
Eryk Ciepiela <eryk.ciepiela.dev@gmail.com>
2017-12-09 17:16:41 +0100

    exercise 2

--- exercise2/src/FizzBuzz.hs
+++ exercise2/src/FizzBuzz.hs
@@ -8,8 +8,8 @@ import Data.Maybe
 program :: IO ()
 program = print $ answer [each 3 "fizz", each 5 "buzz"] show <$> [1..20]
 
+answer :: Monoid s => [a -> Maybe s] -> (a -> s) -> a -> s
+answer maybefs defaultf i = fromMaybe (defaultf i) (mconcat (fmap ($ i) maybefs))
+
 each :: Int -> String -> Int -> Maybe String
 each j s i = if i `mod` j == 0 then Just s else Nothing
-
-answer :: Monoid s => [a -> Maybe s] -> (a -> s) -> a -> s
-answer maybef defaultf i = fromMaybe (defaultf i) (mconcat (fmap ($ i) maybef))
