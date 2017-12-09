9f2b6f20b3245ecdb67e4aaa8458c42bbcbe74ac
Eryk Ciepiela <eryk.ciepiela.dev@gmail.com>
2017-12-09 17:15:27 +0100

    exercise 2

--- exercise2/src/FizzBuzz.hs
+++ exercise2/src/FizzBuzz.hs
@@ -11,14 +11,5 @@ program = print $ answer [each 3 "fizz", each 5 "buzz"] show <$> [1..20]
 each :: Int -> String -> Int -> Maybe String
 each j s i = if i `mod` j == 0 then Just s else Nothing
 
-answer :: [Int -> Maybe String] -> (Int -> String) -> Int -> String
-answer f d i = let x = fmap ($ i) f in fromMaybe (d i) (xxx x)
-  where
-    xxx :: [Maybe String] -> Maybe String
-    xxx [] = Nothing
-    xxx (Just s:mss) = case xxx mss of
-      Nothing -> Just s
-      Just ss -> Just (s ++ ss)
-    xxx (Nothing:mss) = case xxx mss of
-      Nothing -> Nothing
-      Just ss -> Just ss
+answer :: Monoid s => [a -> Maybe s] -> (a -> s) -> a -> s
+answer maybef defaultf i = fromMaybe (defaultf i) (mconcat (fmap ($ i) maybef))
